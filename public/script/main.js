6395f665032f48f4354ee7a5993a9ea9ebdd9762
Sreza <130074829+Rezaabdollahi7@users.noreply.github.com>
2023-08-08 14:27:13 +0330

    end of slogan section and responsive that

--- public/script/main.js
+++ public/script/main.js
@@ -1,3 +1,7 @@
+
+
+
+
 // responsive header in mobile size
 const headerMobileToggleBtn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
@@ -5,11 +9,3 @@ const menu = document.querySelector(".mobile-menu");
 headerMobileToggleBtn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
-
-// Initialization for ES Users
-import {
-    Carousel,
-    initTE,
-} from "tw-elements";
-
-initTE({ Carousel });
