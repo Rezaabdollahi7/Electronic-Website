a0cec67887fd9a28919cee43689c4a1b24d35a5e
Sreza <130074829+Rezaabdollahi7@users.noreply.github.com>
2023-08-12 15:28:42 +0330

    end of features v-1
    add features items , simple logo , lorem text .
    add responsive styles.

--- public/script/main.js
+++ public/script/main.js
@@ -1,7 +1,4 @@
 
-
-
-
 // responsive header in mobile size
 const headerMobileToggleBtn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
@@ -9,3 +6,26 @@ const menu = document.querySelector(".mobile-menu");
 headerMobileToggleBtn.addEventListener("click", () => {
     menu.classList.toggle("hidden");
 });
+
+
+
+// Get the button:
+let callUsbutton = document.getElementById("callUsBtn");
+const scroolVal = 40;
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function () { scrollFunction() };
+
+function scrollFunction() {
+    if (document.body.scrollTop > scroolVal || document.documentElement.scrollTop > scroolVal) {
+        console.log("iam here")
+        callUsbutton.style.display = "block";
+    } else {
+        callUsbutton.style.display = "none";
+    }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+} 
