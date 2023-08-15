5f794d6d4d1acb2e423bd16302f8d58454b737f6
Sreza <130074829+Rezaabdollahi7@users.noreply.github.com>
2023-08-15 11:18:43 +0330

    add new responsive navbar delete aos

--- public/script/main.js
+++ public/script/main.js
@@ -1,11 +1,4 @@
 
-// responsive header in mobile size
-const headerMobileToggleBtn = document.querySelector("button.mobile-menu-button");
-const menu = document.querySelector(".mobile-menu");
-
-headerMobileToggleBtn.addEventListener("click", () => {
-    menu.classList.toggle("hidden");
-});
 
 
 // ============================================================================================
