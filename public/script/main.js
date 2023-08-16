49d940076c866a5654934beb6d9e126d68013b21
Sreza <130074829+Rezaabdollahi7@users.noreply.github.com>
2023-08-16 22:40:11 +0330

    add top product section with swiper js library
    add , responsive , .... v-1

--- public/script/main.js
+++ public/script/main.js
@@ -318,3 +318,31 @@ function cardState(e){
 
     }
 }
+
+// ===================== swiper ===========================
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+        draggable: 'true',
+    },
+    autoplay:{
+        delay:"5000",
+        pauseOnMouseEnter:"true",
+    }
+});
