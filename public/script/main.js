353000da8db7150b5ef8c6bfcc88214942a7e5ad
Rezaabdollahi7 <srezaabdollahi7@gmail.com>
2023-12-02 22:10:00 +0330

    Refactor [Slogan Section] : Add swiper & Responsive

--- public/script/main.js
+++ public/script/main.js
@@ -348,4 +348,31 @@ const swiper = new Swiper('.swiper', {
 
     }
 });
+const swiper2 = new Swiper('.first-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
 
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+    },
+
+    // Navigation arrows
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+
+    // And if we need scrollbar
+    scrollbar: {
+        el: '.swiper-scrollbar',
+    }, autoplay: {
+        delay: "3000",
+        pauseOnMouseEnter: "true",
+    }, pagination: {
+        el: ".swiper-pagination",
+        dynamicBullets: true,
+    },
+
+});
